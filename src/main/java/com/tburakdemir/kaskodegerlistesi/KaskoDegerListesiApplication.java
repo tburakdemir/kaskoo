4bfec8607eaa8ac4595505dd88c5c80a59a21a64
Burak Demir <burakdemir@testeaser.com>
2024-03-21 11:34:18 +0300

    Initial Commit

---
+++ src/main/java/com/tburakdemir/kaskodegerlistesi/KaskoDegerListesiApplication.java
@@ -0,0 +1,13 @@
+package com.tburakdemir.kaskodegerlistesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaskoDegerListesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaskoDegerListesiApplication.class, args);
+	}
+
+}
