bc7b6b3af2e1b52852711955de319975af4cc353
Burak Demir <tburakdemir@gmail.com>
2024-10-06 23:22:43 +0300

    Add next theme

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
